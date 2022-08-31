069d3c5cf8c716554cfa945de9c40990cf453b49
Victória Cardoso <victorialabenu@gmail.com>
2022-08-31 17:28:28 -0300

    exercicios concluidos

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+let nome = prompt ("Qual é seu nome?")
+let idade = prompt("Qual sua idade?")
+idade = Number(idade)
+console. log(typeof(nome))
+console. log(typeof(idade))
+// Para retornar deixei as duas váriaveis como let e logo após converti a variavel idade de string para Number
+
+console.log("Meu nome é", nome)
+console.log("Minha idade é", idade)
+// Para atribuir a
---
+++ exercicio2/script.js
@@ -0,0 +1,24 @@
+
+const variavel1 = prompt("Responda Sim ou Não \nVocê é Swag?")
+console.log(variavel1)
+const variavel2 = prompt("Responda Sim ou Não \nGosta de praia?")
+console.log(variavel2)
+const variavel3 = prompt("Responda Sim ou Não \nJa saltou de paraquedas?") 
+console.log(variavel3)
+
+const perguntaA = "Você é Swag?"
+const respostaA = prompt(perguntaA)
+console.log(respostaA)
+const perguntaB = "Gosta de praia?"
+const respostaB = prompt(perguntaB)
+console.log(respostaB)
+const perguntaC = "Ja saltou de paraquedas?"
+const respostaC = prompt(perguntaC)
+console.log(respostaC)
+
+const a = prompt("Você é Swag?")
+console.log("Você é Swag?", a)
+const b = prompt("Gosta de praia?")
+console.log("Gosta de praia?", b)
+const c = prompt("Ja saltou de paraquedas")
+console.log("Ja saltou de paraquedas?", c)
